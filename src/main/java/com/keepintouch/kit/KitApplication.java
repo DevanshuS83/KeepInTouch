1af4a3058870709ed8eef8f93795ed3a49d66144
Deva <devanshusachdev83@gmai.com>
2025-02-13 16:18:01 +0530

    Initial Git Commit

---
+++ src/main/java/com/keepintouch/kit/KitApplication.java
@@ -0,0 +1,12 @@
+package com.keepintouch.kit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KitApplication.class, args);
+	}
+
+}
